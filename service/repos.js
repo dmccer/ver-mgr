e469e3ba1cb1650ad2ed8dfebae18b79a068ffb9
Kane <dmccer@gmail.com>
2015-07-29 13:47:03 +0800

    添加校验及状态更新

--- service/repos.js
+++ service/repos.js
@@ -3,11 +3,24 @@ var repos = require('../model/repos');
 var logger = require('../logger')('api');
 
 var repos_ctrl = {
+  validate: function(name, owner) {
+    return _.trim(name) && _.trim(owner);
+  },
+
   // 获取单个项目的静态资源信息
   get: function(req, res) {
+    var name = req.params.name;
+    var owner = req.query.owner;
+
+    if (!repos_ctrl.validate(name, owner)) {
+      return res.status(422).json({
+        msg: '缺少参数 name 或 owner'
+      });
+    }
+
     repos.findOne({
-      name: req.params.name,
-      owner: req.query.owner
+      name: name,
+      owner: owner
     }, function(err, doc) {
       if (err) {
         logger.fatal('查询项目静态资源信息失败');
@@ -32,29 +45,58 @@ var repos_ctrl = {
 
   // 添加项目的静态资源信息
   add: function(req, res) {
-    repos.create({
-      name: req.params.name || req.body.name,
-      owner: req.body.owner,
-      url: req.body.url,
-      download: req.body.download,
-      version: req.body.version
-    }, function(err, doc) {
-      if (err || !doc) {
-        logger.fatal('添加项目静态资源信息失败');
+    var name = req.params.name || req.body.name;
+    var owner = req.body.owner;
+
+    if (!repos_ctrl.validate(name, owner)) {
+      return res.status(422).json({
+        msg: '缺少参数 name 或 owner'
+      });
+    }
+
+    repos
+      .findOne({
+        name: name,
+        owner: owner
+      })
+      .exec()
+      .then(function(doc) {
+        if (doc) {
+          throw new Error('项目已存在');
+        }
+
+        return repos
+          .create({
+            name: name,
+            owner: owner,
+            url: req.body.url,
+            download: req.body.download,
+            version: req.body.version,
+            status: req.body.status
+          });
+      }, function(err) {
+        logger.fatal('查询项目静态资源信息失败');
         logger.info(err.message);
 
         return res.status(500).json({
-          msg: '添加项目静态资源信息失败'
+          msg: '查询项目静态资源信息失败'
         });
-      }
+      })
+      .then(function(doc) {
+        logger.info('添加项目静态资源信息成功');
+        logger.info(JSON.stringify(req.body));
 
-      logger.info('添加项目静态资源信息成功');
-      logger.info(JSON.stringify(req.body));
+        return res.status(200).json({
+          data: doc.toObject()
+        });
+      }, function(err) {
+        var err_msg = '添加项目静态资源信息失败, ' + err.message;
+        logger.fatal(err_msg);
 
-      return res.status(200).json({
-        data: doc.toObject()
+        return res.status(500).json({
+          msg: err_msg
+        });
       });
-    });
   },
 
   // 获取项目列表
@@ -86,17 +128,23 @@ var repos_ctrl = {
   update: function(req, res) {
     logger.trace('检测项目是否存在');
 
+    var name = req.params.name;
+    var owner = req.body.owner;
+
     var data = {
-      name: req.params.name,
-      owner: req.body.owner,
-      version: req.body.version,
+      name: name,
+      owner: owner,
       update_time: Date.now()
     };
 
-    if (!_.trim(data.name) || !_.trim(data.owner) || !_.trim(data.versioin)) {
-      return res.status(500).json({
-        msg: '缺少必传参数 name, owner, version'
+    if (!repos_ctrl.validate(name, owner)) {
+      return res.status(422).json({
+        msg: '缺少参数 name 或 owner'
       });
+    }
+
+    if (req.body.version) {
+      data.version = req.body.version;
     }
 
     if (req.body.url) {
@@ -107,8 +155,15 @@ var repos_ctrl = {
       data.download = req.body.download;
     }
 
-    // 这里有个bug
-    // create_time 会更新
+    if (req.body.status) {
+      data.status = req.body.status;
+    }
+
+    // create_time 仅在添加时写入
+    data.$setOnInsert = {
+      create_time: new Date()
+    };
+
     repos.findOneAndUpdate({
       name: data.name,
       owner: data.owner
